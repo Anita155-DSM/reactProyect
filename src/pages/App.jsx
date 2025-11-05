8faea693cd497208b463ea8c7cce882b51e7010d
Anahi Perez <anahiperezdsm@gmail.com>
2025-11-05 18:05:23 -0300

    comienzo la tarea 03 - agrego un boton en App.jsx con intención de que cuando se apriete, muestre la tarea 03

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Login } from "../components/Login";
 import { Register } from "../components/register";
+import { MultipleCustomHooks } from "./MultipleCustomHooks";
 //DISCULPE EL DESARROLLO DESORDENADO, ESTOY PRACTICANDO MUCHO CON LOS HOOKS Y ME PIERDO UN POCO.
 //OLVIDÉ CREAR MI REPOSITORIO NUEVO PARA LOS EJERCICIOS. NO VOLVERÁ A PASAR.
 export const App = () => {
@@ -12,8 +13,10 @@ export const App = () => {
 
   const handleLogout = () => setUser("");
 
+  const [showSimpsons, setShowSimpsons] = useState(false);
+
   return (
-    <>
+    <> 
   <h1>¡Hola de nuevo {user}!</h1>
   <button onClick={handleLogout}>Logout</button>
 
@@ -22,6 +25,13 @@ export const App = () => {
     Register. si no tenés una cuenta, registrate
   </h2>
   <Register />
+  <div>
+    <button onClick={() => setShowSimpsons((v) => !v)}>
+      {showSimpsons ? "Ocultar API Los Simpsons" : "Tarea 03 - API Los Simpsons"}
+    </button>
+  </div>
+
+  {showSimpsons && <MultipleCustomHooks />}
     </>
   );
 };
